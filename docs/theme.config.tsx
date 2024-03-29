2ec192c8948273e859519520db342cfaae173950
chanhyuk-park <chanhyuk.park@toss.im>
2024-03-30 00:20:23 +0900

    docs폴더에 포함된 .git config file을 제거합니다

---
+++ docs/theme.config.tsx
@@ -0,0 +1,88 @@
+import { useRouter } from 'next/router';
+import type { DocsThemeConfig } from 'nextra-theme-docs';
+import { useConfig } from 'nextra-theme-docs';
+
+const config: DocsThemeConfig = {
+  project: {
+    link: 'https://github.com/toss/es-hangul',
+  },
+  docsRepositoryBase: 'https://github.com/stoss/es-hangul/tree/main/docs',
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – es-hangul',
+      };
+    }
+  },
+  logo: <span>es-hangul</span>,
+  head: function useHead() {
+    const { title } = useConfig();
+    const { route } = useRouter();
+    const socialCard =
+      route === '/' || !title ? 'https://nextra.site/og.jpeg' : `https://nextra.site/api/og?title=${title}`;
+
+    return (
+      <>
+        <meta name="msapplication-TileColor" content="#fff" />
+        <meta name="theme-color" content="#fff" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="Content-Language" content="en" />
+        <meta name="description" content="Make beautiful websites with Next.js & MDX." />
+        <meta name="og:description" content="Make beautiful websites with Next.js & MDX." />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content={socialCard} />
+        <meta name="twitter:site:domain" content="nextra.site" />
+        <meta name="twitter:url" content="https://nextra.site" />
+        <meta name="og:title" content={title ? title + ' – Nextra' : 'Nextra'} />
+        <meta name="og:image" content={socialCard} />
+        <meta name="apple-mobile-web-app-title" content="Nextra" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link rel="icon" href="/favicon-dark.svg" type="image/svg+xml" media="(prefers-color-scheme: dark)" />
+        <link rel="icon" href="/favicon-dark.png" type="image/png" media="(prefers-color-scheme: dark)" />
+      </>
+    );
+  },
+  editLink: {
+    text: 'Edit this page on GitHub →',
+  },
+  feedback: {
+    content: 'Question? Give us feedback →',
+    labels: 'feedback',
+  },
+  sidebar: {
+    titleComponent({ title, type }) {
+      if (type === 'separator') {
+        return <span className="cursor-default">{title}</span>;
+      }
+      return <>{title}</>;
+    },
+    defaultMenuCollapseLevel: 1,
+    toggleButton: true,
+  },
+  footer: {
+    text: (
+      <div className="flex w-full flex-col items-center sm:items-start">
+        <div>
+          <a
+            className="flex items-center gap-1 text-current"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="vercel.com homepage"
+            href="https://vercel.com?utm_source=nextra.site"
+          >
+            <span>Powered by</span>
+            <img src="/toss-logo.png" alt="Vercel.com" width="70" />
+          </a>
+        </div>
+        <p className="mt-6 text-xs">© {new Date().getFullYear()} The Opensource Project.</p>
+      </div>
+    ),
+  },
+  toc: {
+    backToTop: true,
+  },
+};
+
+export default config;
