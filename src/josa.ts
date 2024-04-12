ed6fb92a078e24028556d6f8e9bc74b85a22f58d
류정우 <evencoding@gmail.com>
2024-04-13 08:25:46 +0900

    feat: 단어가 완전한 괄호로 끝난다면 괄호를 제거 후 조사 결정

--- src/josa.ts
+++ src/josa.ts
@@ -19,20 +19,25 @@ type JosaOption =
 const 로_조사: JosaOption[] = ['으로/로', '으로서/로서', '으로써/로써', '으로부터/로부터'];
 
 export function josa(word: string, josa: JosaOption): string {
-  if (word.length === 0) {
+  const wordWithoutLastBracket = removeLast완전한괄호(word);
+
+  if (wordWithoutLastBracket.length === 0) {
     return word;
   }
 
-  return word + josaPicker(word, josa);
+  return word + josaPicker(wordWithoutLastBracket, josa);
 }
 
 josa.pick = josaPicker;
 
 function josaPicker(word: string, josa: JosaOption): string {
-  const has받침 = hasBatchim(word);
-  let index = has받침 ? 0 : 1;
+  const wordWithoutLastBracket = removeLast완전한괄호(word);
 
-  const is종성ㄹ = disassembleCompleteHangulCharacter(word[word.length - 1]!)?.last === 'ㄹ';
+  const has받침 = hasBatchim(wordWithoutLastBracket);
+  let index = has받침 ? 0 : 1;
+  const lastChar = wordWithoutLastBracket[wordWithoutLastBracket.length - 1];
+
+  const is종성ㄹ = disassembleCompleteHangulCharacter(lastChar!)?.last === 'ㄹ';
 
   const isCaseOf로 = has받침 && is종성ㄹ && 로_조사.includes(josa);
 
@@ -40,11 +45,25 @@ function josaPicker(word: string, josa: JosaOption): string {
     index = index === 0 ? 1 : 0;
   }
 
-  const isEndsWith이 = word[word.length - 1] === '이';
+  const isEndsWith이 = lastChar === '이';
 
   if (josa === '이에요/예요' && isEndsWith이) {
     index = 1;
   }
 
   return josa.split('/')[index]!;
+}
+
+function removeLast완전한괄호(word: string) {
+  const endsWith완전한괄호 = checkEndsWith완전한괄호(word);
+
+  if (!endsWith완전한괄호) {
+    return word;
+  }
+
+  return word.replace(/\([^()]*\)(?=[^()]*$)/, '').trim();
+}
+
+function checkEndsWith완전한괄호(word: string) {
+  return /\([^)]*\)$/.test(word);
 }
