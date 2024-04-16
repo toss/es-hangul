16645753056faa9702be5c50f1042618c032b186
Evan Moon <bboydart91@gmail.com>
2024-04-16 20:47:17 +0900

    feat: 인자로 받은 문자의 초성, 중성, 종성 위치 가능 여부를 파악하는 함수 추가 (#36)
    * feat: 인자로 받은 문자의 초성, 중성, 종성 위치 가능 여부를 파악하는 함수 추가
    
    * Create sweet-fireants-cough.md

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, expectTypeOf, it } from 'vitest';
-import { getFirstConsonants, hasBatchim, hasProperty, hasValueInReadOnlyStringList } from './utils';
+import { canBeChosung, canBeJongsung, canBeJungsung, getFirstConsonants, hasBatchim, hasProperty, hasValueInReadOnlyStringList } from './utils';
 
 describe('hasBatchim', () => {
   it('should return true for the character "값"', () => {
@@ -87,5 +87,68 @@ describe('hasProperty', () => {
     } else {
       expectTypeOf(testKey).toEqualTypeOf<string>();
     }
+  });
+});
+
+describe('canBeChosung', () => {
+  it('ㄱ', () => {
+    expect(canBeChosung('ㄱ')).toBe(true);
+  });
+  it('ㅃ', () => {
+    expect(canBeChosung('ㅃ')).toBe(true);
+  });
+  it('ㅏ', () => {
+    expect(canBeChosung('ㅏ')).toBe(false);
+  });
+  it('ㅘ', () => {
+    expect(canBeChosung('ㅏ')).toBe(false);
+  });
+  it('ㄱㅅ', () => {
+    expect(canBeChosung('ㅏ')).toBe(false);
+  });
+  it('가', () => {
+    expect(canBeChosung('ㅏ')).toBe(false);
+  });
+});
+
+describe('canBeJungsung', () => {
+  it('ㅗㅏ', () => {
+    expect(canBeJungsung('ㅗㅏ')).toBe(true);
+  });
+  it('ㅏ', () => {
+    expect(canBeJungsung('ㅏ')).toBe(true);
+  });
+  it('ㄱ', () => {
+    expect(canBeJungsung('ㄱ')).toBe(false);
+  });
+  it('ㄱㅅ', () => {
+    expect(canBeJungsung('ㄱㅅ')).toBe(false);
+  });
+  it('가', () => {
+    expect(canBeJungsung('가')).toBe(false);
+  });
+});
+
+describe('canBeJongsung', () => {
+  it('ㄱ', () => {
+    expect(canBeJongsung('ㄱ')).toBe(true);
+  });
+  it('ㄱㅅ', () => {
+    expect(canBeJongsung('ㄱㅅ')).toBe(true);
+  });
+  it('ㅂㅅ', () => {
+    expect(canBeJongsung('ㅂㅅ')).toBe(true);
+  });
+  it('ㅎㄹ', () => {
+    expect(canBeJongsung('ㅎㄹ')).toBe(false);
+  });
+  it('ㅗㅏ', () => {
+    expect(canBeJongsung('ㅗㅏ')).toBe(false);
+  });
+  it('ㅏ', () => {
+    expect(canBeJongsung('ㅏ')).toBe(false);
+  });
+  it('가', () => {
+    expect(canBeJongsung('ㅏ')).toBe(false);
   });
 });
