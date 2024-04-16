0057cfffd7b32ab6c0eb188b6cf20c2e7c86ccbe
evan-moon <bboydart91@gmail.com>
2024-04-16 22:04:42 +0900

    chore: jsDoc 추가

--- src/combineHangulCharacter.ts
+++ src/combineHangulCharacter.ts
@@ -7,6 +7,24 @@ import {
 } from './constants';
 import { canBeChosung, canBeJongsung, canBeJungsung } from './utils';
 
+/**
+ * @name combineHangulCharacter
+ * @description
+ * 인자로 초성, 중성, 종성을 받아 하나의 한글 문자를 반환합니다.
+ * ```typescript
+ * combineHangulCharacter(
+ *   // 초성
+ *   firstCharacter: string
+ *   // 중성
+ *   middleCharacter: string
+ *   // 종성
+ *   lastCharacter: string
+ * ): string
+ * ```
+ * @example
+ * combineHangulCharacter('ㄱ', 'ㅏ', 'ㅂㅅ') // '값'
+ * combineHangulCharacter('ㅌ', 'ㅗ') // '토'
+ */
 export function combineHangulCharacter(firstCharacter: string, middleCharacter: string, lastCharacter = '') {
   if (
     canBeChosung(firstCharacter) === false ||
@@ -29,12 +47,38 @@ export function combineHangulCharacter(firstCharacter: string, middleCharacter: 
   return String.fromCharCode(unicode);
 }
 
+/**
+ * @name curriedCombineHangulCharacter
+ * @description
+ * 인자로 초성, 중성, 종성을 받아 하나의 한글 문자를 반환하는 `combineHangulCharacter` 함수의 커링된 버전입니다.
+ * @example
+ * const combineMiddleHangulCharacter = curriedCombineHangulCharacter('ㄱ')
+ * const combineLastHangulCharacter = combineMiddleHangulCharacter('ㅏ')
+ * combineLastHangulCharacter('ㄱ') // '각'
+ */
 export const curriedCombineHangulCharacter =
   (firstCharacter: string) =>
   (middleCharacter: string) =>
   (lastCharacter = '') =>
     combineHangulCharacter(firstCharacter, middleCharacter, lastCharacter);
 
+/**
+ * @name combineVowels
+ * @description
+ * 인자로 두 개의 모음을 받아 합성하여 겹모음을 생성합니다. 만약 올바른 한글 규칙으로 합성할 수 없는 모음들이라면 단순 Join합니다.
+ * ```typescript
+ * combineVowels(
+ *   // 첫 번째 모음
+ *   vowel1: string
+ *   // 두 번째 모음
+ *   vowel2: string
+ * ): string
+ * ```
+ * @example
+ * combineVowels('ㅗ', 'ㅏ') // 'ㅘ'
+ * combineVowels('ㅗ', 'ㅐ') // 'ㅙ'
+ * combineVowels('ㅗ', 'ㅛ') // 'ㅗㅛ'
+ */
 export const combineVowels = (vowel1: string, vowel2: string) =>
   Object.entries(DISASSEMBLED_VOWELS_BY_VOWEL).find(([, value]) => value === `${vowel1}${vowel2}`)?.[0] ??
   `${vowel1}${vowel2}`;
