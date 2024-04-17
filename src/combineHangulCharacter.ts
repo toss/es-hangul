1e2d292ac53209fa0941f6a7ff0510617a82557f
evan-moon <bboydart91@gmail.com>
2024-04-17 10:35:31 +0900

    refactor: 변수명을 영어로 변경

--- src/combineHangulCharacter.ts
+++ src/combineHangulCharacter.ts
@@ -34,15 +34,18 @@ export function combineHangulCharacter(firstCharacter: string, middleCharacter: 
     throw new Error(`Invalid hangul Characters: ${firstCharacter}, ${middleCharacter}, ${lastCharacter}`);
   }
 
-  const 중성개수 = HANGUL_CHARACTERS_BY_MIDDLE_INDEX.length;
-  const 종성개수 = HANGUL_CHARACTERS_BY_LAST_INDEX.length;
+  const numOfMiddleCharacters = HANGUL_CHARACTERS_BY_MIDDLE_INDEX.length;
+  const numOfLastCharacters = HANGUL_CHARACTERS_BY_LAST_INDEX.length;
 
-  const 초성인덱스 = HANGUL_CHARACTERS_BY_FIRST_INDEX.indexOf(firstCharacter);
-  const 중성인덱스 = HANGUL_CHARACTERS_BY_MIDDLE_INDEX.indexOf(middleCharacter);
-  const 종성인덱스 = HANGUL_CHARACTERS_BY_LAST_INDEX.indexOf(lastCharacter);
+  const firstCharacterIndex = HANGUL_CHARACTERS_BY_FIRST_INDEX.indexOf(firstCharacter);
+  const middleCharacterIndex = HANGUL_CHARACTERS_BY_MIDDLE_INDEX.indexOf(middleCharacter);
+  const lastCharacterIndex = HANGUL_CHARACTERS_BY_LAST_INDEX.indexOf(lastCharacter);
+
+  const firstIndexOfTargetConsonant = firstCharacterIndex * numOfMiddleCharacters * numOfLastCharacters;
+  const firstIndexOfTargetVowel = middleCharacterIndex * numOfLastCharacters;
 
   const unicode =
-    COMPLETE_HANGUL_START_CHARCODE + 초성인덱스 * 중성개수 * 종성개수 + 중성인덱스 * 종성개수 + 종성인덱스;
+    COMPLETE_HANGUL_START_CHARCODE + firstIndexOfTargetConsonant + firstIndexOfTargetVowel + lastCharacterIndex;
 
   return String.fromCharCode(unicode);
 }
