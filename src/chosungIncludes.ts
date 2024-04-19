bc310833126dae9f0fdcb3500c3f8f2cae4cc824
bosung baek <teabs1125@gmail.com>
2024-04-19 10:57:21 +0900

    refactor: 공백을 제거하는 역할을 chosungIncludes에 맡기도록 개선

--- src/chosungIncludes.ts
+++ src/chosungIncludes.ts
@@ -2,23 +2,23 @@ import { disassembleHangulToGroups } from './disassemble';
 import { canBeChosung, getFirstConsonants } from './utils';
 
 export function chosungIncludes(x: string, y: string) {
-  if (!isOnlyInitialConsonant(y)) {
+  const trimY = y.replace(/\s/g, '');
+
+  if (!isOnlyInitialConsonant(trimY)) {
     return false;
   }
 
   const initialConsonantsX = getFirstConsonants(x).replace(/\s/g, '');
-  const initialConsonantsY = getFirstConsonants(y).replace(/\s/g, '');
+  const initialConsonantsY = trimY;
 
   return initialConsonantsX.includes(initialConsonantsY);
 }
 
 /*
- * @description 공백을 제외한 문자열이 한글초성으로만 주어진 경우
+ * @description 문자열이 한글초성으로만 주어진 경우
  */
 function isOnlyInitialConsonant(str: string) {
-  const trimStr = str.replace(/\s/g, '');
-
-  return disassembleHangulToGroups(trimStr).every(disassembled => {
+  return disassembleHangulToGroups(str).every(disassembled => {
     return disassembled.length === 1 && canBeChosung(disassembled[0]);
   });
 }
