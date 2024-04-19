f668e15318744e2c0195a826ef0d963de26072cb
BOSUNG BAEK <78058734+BO-LIKE-CHICKEN@users.noreply.github.com>
2024-04-19 12:25:46 +0900

    fix: Handle spaces in initialConsonants search inputs properly (#42)
    * fix: Handle spaces in initialConsonants search inputs properly
    
    * test: Add test cases for initialConsonants search with space
    
    * fix: remove unused function
    
    * refactor: 공백을 제거하는 역할을 chosungIncludes에 맡기도록 개선
    
    * test: 초성자리에 공백만 들어갈 경우에 대한 테스트 케이스 추가
    
    * fix: 초성 자리에 공백만 들어간 예외사항 처리
    
    * refactor: trim된 문자열을 trimmedStr로 변경
    
    Co-authored-by: 박찬혁 <pgg6713@gmail.com>
    
    * fix: 누락된 trimmedY의 변수명 변경 반영
    
    * Create kind-pianos-thank.md
    
    ---------
    
    Co-authored-by: 박찬혁 <pgg6713@gmail.com>

--- src/chosungIncludes.spec.ts
+++ src/chosungIncludes.spec.ts
@@ -14,6 +14,18 @@ describe('chosungIncludes', () => {
     expect(chosungIncludes('프론트엔드', 'ㅍㅌ')).toBe(false);
   });
 
+  it('should return true when "ㅍㄹㅌㅇㄷㄱㅂㅈ" is entered for searching "프론트엔드 개발자"', () => {
+    expect(chosungIncludes('프론트엔드 개발자', 'ㅍㄹㅌㅇㄷㄱㅂㅈ')).toBe(true);
+  });
+
+  it('should return true when "ㅍㄹㅌㅇㄷ ㄱㅂㅈ" is entered for searching "프론트엔드 개발자"', () => {
+    expect(chosungIncludes('프론트엔드 개발자', 'ㅍㄹㅌㅇㄷ ㄱㅂㅈ')).toBe(true);
+  });
+
+  it('should return true when "ㅍㄹㅌㅇㄷ ㄱㅂㅈ" is entered for searching " "', () => {
+    expect(chosungIncludes('프론트엔드 개발자', ' ')).toBe(false);
+  });
+
   it('should return false when "푸롴트" is entered for searching "프론트엔드" as it does not only include the initial consonants.', () => {
     expect(chosungIncludes('프론트엔드', '푸롴트')).toBe(false);
   });
