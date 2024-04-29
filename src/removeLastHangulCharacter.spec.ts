36313f9854a0f0efe02e4970f5b97dcce33c6ea8
김관식 <39869096+gwansikk@users.noreply.github.com>
2024-04-29 14:33:09 +0900

    test: 테스트 한글화와 테스트 커버리지 개선  (#81)
    * test: 테스트 한글화
    
    * test: Add test case for empty string input in josa function
    
    * test: Add test case for empty string input in removeLastHangulCharacter function
    
    * test: Add tests for getFirstConsonants function

--- src/removeLastHangulCharacter.spec.ts
+++ src/removeLastHangulCharacter.spec.ts
@@ -1,14 +1,17 @@
 import { removeLastHangulCharacter } from './removeLastHangulCharacter';
 
 describe('removeLastHangulCharacter', () => {
-  it('마지막 문자가 겹받침인 경우 홑받침으로 바꾼다', () => {
+  it('마지막 문자가 겹받침인 경우 홑받침으로 바꾼다.', () => {
     expect(removeLastHangulCharacter('안녕하세요 값')).toBe('안녕하세요 갑');
   });
-  it('마지막 문자가 초성과 중성의 조합으로 끝날 경우 초성만 남긴다', () => {
+  it('마지막 문자가 초성과 중성의 조합으로 끝날 경우 초성만 남긴다.', () => {
     expect(removeLastHangulCharacter('프론트엔드')).toBe('프론트엔ㄷ');
   });
   it('마지막 문자가 초성과 중성과 종성의 조합으로 끝날 경우 초성과 중성이 조합된 문자만 남긴다.', () => {
     expect(removeLastHangulCharacter('일요일')).toBe('일요이');
     expect(removeLastHangulCharacter('깎')).toBe('까');
+  });
+  it('빈 문자열일 경우 빈 문자열을 반환한다.', () => {
+    expect(removeLastHangulCharacter('')).toBe('');
   });
 });
