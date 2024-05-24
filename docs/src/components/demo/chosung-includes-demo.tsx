74d8774c10abe97c5f46ed184dd1890bb0c7a354
chanhyuk-park <chanhyuk.park@toss.im>
2024-05-24 21:36:31 +0900

    remove useEffect in import statement

--- docs/src/components/demo/chosung-includes-demo.tsx
+++ docs/src/components/demo/chosung-includes-demo.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { chosungIncludes } from 'es-hangul';
 
 export function ChosungIncludesDemo() {
