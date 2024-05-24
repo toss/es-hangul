de54a60bc30a55d5d3f3f910bfcb4508c4e947d0
chanhyuk-park <chanhyuk.park@toss.im>
2024-05-24 17:17:04 +0900

    add imweb

--- docs/src/pages/docs/introduction.constatns.ts
+++ docs/src/pages/docs/introduction.constatns.ts
@@ -6,4 +6,11 @@ export const adopters = [
       dark: 'https://framerusercontent.com/images/9DjROJdBAEConeKkYzErikQ2GGo.png?scale-down-to=512',
     },
   },
+  {
+    name: '아임웹',
+    image: {
+      light: 'https://github.com/toss/es-hangul/assets/69495129/284ca4be-6ddf-4fd4-978c-b48dbca5c3b7',
+      dark: 'https://github.com/toss/es-hangul/assets/69495129/284ca4be-6ddf-4fd4-978c-b48dbca5c3b7',
+    },
+  },
 ];
