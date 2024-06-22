f73194df6b3bf6fe9cf11f4339e38648b16650a7
crucify <shj@xenosi.de>
2024-06-23 02:43:52 +0900

    fix: 변환 가능 최대 수 가독성 개선

--- src/amountToHangul.ts
+++ src/amountToHangul.ts
@@ -1,4 +1,5 @@
 export const HANGUL_DIGITS = ['', '만', '억', '조', '경', '해', '자', '양', '구', '간', '정', '재', '극', '항하사', '아승기', '나유타', '불가사의', '무량대수', '겁', '업'];
+export const HANGUL_DIGITS_MAX = HANGUL_DIGITS.length * 4;
 export const HANGUL_NUMBERS = ['', '일', '이', '삼', '사', '오', '육', '칠', '팔', '구'];
 export const HANGUL_CARDINAL = ['', '십', '백', '천'];
 
@@ -7,8 +8,8 @@ export const HANGUL_CARDINAL = ['', '십', '백', '천'];
 export function amountToHangul(str: string) {
   str = str.replace(/\..*$/, '') // 소수점 지원 안함
     .replace(/[^\d]+/g, ''); // , 표기 등 오류내지 않음
-  if(str.length > 80) {
-    return undefined;
+  if(str.length > HANGUL_DIGITS_MAX) {
+    throw new Error('convert range exceeded : ' + str);
   }
   const result = [];
   for(let i = 0; i < str.length - 1; i ++) {
