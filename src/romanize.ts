5da65f6f26240715ef0ac807256c42343bd1aac4
po4tion <po4tion0429@gmail.com>
2024-06-26 11:14:36 +0900

    fix: romanize 가독성을 개선합니다

--- src/romanize.ts
+++ src/romanize.ts
@@ -10,39 +10,44 @@ import { standardPronunciation } from './standardPronunciation';
  */
 export function romanize(hangul: string): string {
   const changedHangul = standardPronunciation(hangul, { hardConversion: false });
-  let roman = '';
 
-  for (let i = 0; i < changedHangul.length; i += 1) {
-    const syllable = changedHangul[i];
+  return changedHangul
+    .split('')
+    .map((_, i, arrayHangul) => romanizeSyllableHangul(arrayHangul, i))
+    .join('');
+}
 
-    if (isHangulCharacter(syllable)) {
-      const disassembledHangul = disassembleCompleteHangulCharacter(syllable) as NonNullable<
-        ReturnType<typeof disassembleCompleteHangulCharacter>
-      >;
+const romanizeSyllableHangul = (arrayHangul: string[], index: number): string => {
+  const syllable = arrayHangul[index];
 
-      let chosung: (typeof 초성_알파벳_발음)[keyof typeof 초성_알파벳_발음] | 'l' =
-        초성_알파벳_발음[disassembledHangul.first];
-      const jungsung = 중성_알파벳_발음[assembleHangul([disassembledHangul.middle]) as keyof typeof 중성_알파벳_발음];
-      const jongsung = 종성_알파벳_발음[disassembledHangul.last as keyof typeof 종성_알파벳_발음];
+  if (isHangulCharacter(syllable)) {
+    const disassemble = disassembleCompleteHangulCharacter(syllable) as NonNullable<
+      ReturnType<typeof disassembleCompleteHangulCharacter>
+    >;
 
-      // 'ㄹ'은 모음 앞에서는 'r'로, 자음 앞이나 어말에서는 'l'로 적는다. 단, 'ㄹㄹ'은 'll'로 적는다. (ex.울릉, 대관령),
-      if (disassembledHangul.first === 'ㄹ' && i > 0 && isHangulCharacter(changedHangul[i - 1])) {
-        const prevDisassembledHangul = disassembleCompleteHangulCharacter(changedHangul[i - 1]);
+    let chosung: (typeof 초성_알파벳_발음)[keyof typeof 초성_알파벳_발음] | 'l' = 초성_알파벳_발음[disassemble.first];
+    const jungsung = 중성_알파벳_발음[assembleHangul([disassemble.middle]) as keyof typeof 중성_알파벳_발음];
+    const jongsung = 종성_알파벳_발음[disassemble.last as keyof typeof 종성_알파벳_발음];
 
-        if (prevDisassembledHangul?.last === 'ㄹ') {
-          chosung = 'l';
-        }
+    // 'ㄹ'은 모음 앞에서는 'r'로, 자음 앞이나 어말에서는 'l'로 적는다. 단, 'ㄹㄹ'은 'll'로 적는다. (ex.울릉, 대관령),
+    if (disassemble.first === 'ㄹ' && index > 0 && isHangulCharacter(arrayHangul[index - 1])) {
+      const prevDisassemble = disassembleCompleteHangulCharacter(arrayHangul[index - 1]);
+
+      if (prevDisassemble?.last === 'ㄹ') {
+        chosung = 'l';
       }
-
-      roman += chosung + jungsung + jongsung;
-    } else if (syllable in 중성_알파벳_발음) {
-      roman += 중성_알파벳_발음[syllable as keyof typeof 중성_알파벳_발음];
-    } else if (canBeChosung(syllable)) {
-      roman += 초성_알파벳_발음[syllable as keyof typeof 초성_알파벳_발음];
-    } else {
-      roman += syllable;
     }
+
+    return chosung + jungsung + jongsung;
   }
 
-  return roman;
-}
+  if (syllable in 중성_알파벳_발음) {
+    return 중성_알파벳_발음[syllable as keyof typeof 중성_알파벳_발음];
+  }
+
+  if (canBeChosung(syllable)) {
+    return 초성_알파벳_발음[syllable as keyof typeof 초성_알파벳_발음];
+  }
+
+  return syllable;
+};
