ba3ed2aeeff4b69920b3df27848fd1f6e8420f27
taehyun <126179088+KNU-K@users.noreply.github.com>
2024-06-29 22:41:14 +0900

    Update src/getHangulAcronym.ts
    Co-authored-by: 박찬혁 <pgg6713@gmail.com>

--- src/getHangulAcronym.ts
+++ src/getHangulAcronym.ts
@@ -7,7 +7,7 @@ import { isHangul } from './_internal/hangul';
  * 한글 문장을 입력받아서, 해당 한글 문장의 초성을을 리턴해줍니다.
  * 한글 문장이 아닌, 문장은 취급하지않습니다. 추가로 한글 문장 + 영어 문장의 경우에도 취급하지않습니다.
  */
-export function getHangulAcronym(text: string) {
+export function getHangulAcronym(hangul: string) {
   if (!isHangul(text)) {
     throw new Error('Invalid Hangul text, please input Hangul text only.');
   }
