b31a5b398baa1d5d9e6adf286c4dd844ea63d0e4
KNU-K <knukang334@gmail.com>
2024-06-29 21:50:18 +0900

    fix : rename function
    getFirstHangulLetters -> getHangulAcronym

--- src/getHangulAcronym.ts
+++ src/getHangulAcronym.ts
@@ -1,14 +1,14 @@
-import { isHangulText } from './_internal/hangul';
+import { isHangulOnly } from './_internal/hangul';
 
 /**
  *
- * @param getFirstHangulLetters
+ * @param getHangulAcronym
  * @description
  * 한글 문장을 입력받아서, 해당 한글 문장의 초성을을 리턴해줍니다.
  * 한글 문장이 아닌, 문장은 취급하지않습니다. 추가로 한글 문장 + 영어 문장의 경우에도 취급하지않습니다.
  */
-export function getFirstHangulLetters(text: string) {
-  if (!isHangulText(text)) throw new Error('Invalid Hangul text, please input Hangul text only.');
+export function getHangulAcronym(text: string) {
+  if (!isHangulOnly(text)) throw new Error('Invalid Hangul text, please input Hangul text only.');
 
   const words = text.split(' ');
 
