ff74a29e422a85af7df957b7466f1cec1991bac5
SeongMin Kim <86355699+Collection50@users.noreply.github.com>
2024-06-30 10:57:36 +0900

    fix: amountToHangul이 소수점, 숫자도 대응할 수 있도록 수정 (#144)
    * fix: amountToHangul이 소수점, 숫자도 대응할 수 있도록 수정
    
    * docs: amountToHangul의 영어 문서 작성
    
    * Create famous-cheetahs-sneeze.md
    
    * Update famous-cheetahs-sneeze.md
    
    ---------
    
    Co-authored-by: 박찬혁 <pgg6713@gmail.com>

--- src/amountToHangul.ts
+++ src/amountToHangul.ts
@@ -1,35 +1,71 @@
-export const HANGUL_DIGITS = ['', '만', '억', '조', '경', '해', '자', '양', '구', '간', '정', '재', '극', '항하사', '아승기', '나유타', '불가사의', '무량대수', '겁', '업'];
+export const HANGUL_DIGITS = [
+  '',
+  '만',
+  '억',
+  '조',
+  '경',
+  '해',
+  '자',
+  '양',
+  '구',
+  '간',
+  '정',
+  '재',
+  '극',
+  '항하사',
+  '아승기',
+  '나유타',
+  '불가사의',
+  '무량대수',
+  '겁',
+  '업',
+];
 export const HANGUL_DIGITS_MAX = HANGUL_DIGITS.length * 4;
 export const HANGUL_NUMBERS = ['', '일', '이', '삼', '사', '오', '육', '칠', '팔', '구'];
 export const HANGUL_CARDINAL = ['', '십', '백', '천'];
 
-// https://ko.dict.naver.com/#/correct/korean/info?seq=602
-// https://github.com/crucifyer/koreanCardinalOrdinal
-export function amountToHangul(str: string) {
-  str = str.replace(/\..*$/, '') // 소수점 지원 안함
-    .replace(/[^\d]+/g, ''); // , 표기 등 오류내지 않음
-  if(str.length > HANGUL_DIGITS_MAX) {
-    throw new Error('convert range exceeded : ' + str);
+export function amountToHangul(amount: string | number) {
+  const [integerPart, decimalPart] = String(amount)
+    .replace(/[^\d.]+/g, '')
+    .split('.');
+
+  if (integerPart.length > HANGUL_DIGITS_MAX) {
+    throw new Error(`convert range exceeded : ${amount}`);
   }
+
   const result = [];
   let pronunDigits = true;
-  for(let i = 0; i < str.length - 1; i ++) {
-    const d = str.length - i - 1;
-    if(str[i] > '1' || d % 4 === 0 || i === 0) {
-      const tnum = HANGUL_NUMBERS[parseInt(str[i])];
-      if(tnum) {
-        result.push(tnum);
+
+  for (let i = 0; i < integerPart.length - 1; i++) {
+    const digit = integerPart.length - i - 1;
+
+    if (integerPart[i] > '1' || digit % 4 === 0 || i === 0) {
+      const hangulNumber = HANGUL_NUMBERS[Number(integerPart[i])];
+
+      if (hangulNumber) {
+        result.push(hangulNumber);
         pronunDigits = true;
       }
     }
-    if(pronunDigits && d % 4 === 0) {
-      result.push(HANGUL_DIGITS[d / 4]);
+
+    if (pronunDigits && digit % 4 === 0) {
+      result.push(HANGUL_DIGITS[digit / 4]);
       pronunDigits = false;
     }
-    if(str[i] !== '0') {
-      result.push(HANGUL_CARDINAL[d % 4]);
+
+    if (integerPart[i] !== '0') {
+      result.push(HANGUL_CARDINAL[digit % 4]);
     }
   }
-  result.push(HANGUL_NUMBERS[parseInt(str[str.length - 1])]);
+  result.push(HANGUL_NUMBERS[Number(integerPart[integerPart.length - 1])]);
+
+  if (decimalPart) {
+    result.push('점');
+
+    for (let i = 0; i < decimalPart.length; i++) {
+      result.push(HANGUL_NUMBERS[Number(decimalPart[i])]);
+    }
+  }
+
   return result.join('');
 }
