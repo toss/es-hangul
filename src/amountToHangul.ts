a735b1150daa406aebdc3b46a2f3bc1d5dc1d072
Song Hyo Jin <shj@xenosi.de>
2024-07-03 15:35:00 +0900

    fix: 소수점 추가 후 발생한 '영'읽기 버그 수정 (#159)
    * fix: 소수점 추가 후 발생한 '영'읽기 버그 수정
    
    * Create big-cups-call.md
    
    ---------
    
    Co-authored-by: 박찬혁 <pgg6713@gmail.com>

--- src/amountToHangul.ts
+++ src/amountToHangul.ts
@@ -22,48 +22,54 @@ export const HANGUL_DIGITS = [
 ];
 export const HANGUL_DIGITS_MAX = HANGUL_DIGITS.length * 4;
 export const HANGUL_NUMBERS = ['', '일', '이', '삼', '사', '오', '육', '칠', '팔', '구'];
+export const HANGUL_NUMBERS_FOR_DECIMAL = ['영', '일', '이', '삼', '사', '오', '육', '칠', '팔', '구'];
 export const HANGUL_CARDINAL = ['', '십', '백', '천'];
 
 export function amountToHangul(amount: string | number) {
-  const [integerPart, decimalPart] = String(amount)
+  const [integerPart, tempDecimalPart] = String(amount)
     .replace(/[^\d.]+/g, '')
     .split('.');
 
   if (integerPart.length > HANGUL_DIGITS_MAX) {
     throw new Error(`convert range exceeded : ${amount}`);
   }
+  const decimalPart = tempDecimalPart?.replace(/0+$/, '');
 
   const result = [];
   let pronunDigits = true;
 
-  for (let i = 0; i < integerPart.length - 1; i++) {
-    const digit = integerPart.length - i - 1;
+  if(integerPart === '0' || (integerPart === '' && tempDecimalPart)) {
+    result.push(HANGUL_NUMBERS_FOR_DECIMAL[0]);
+  } else {
+    for (let i = 0; i < integerPart.length - 1; i++) {
+      const digit = integerPart.length - i - 1;
 
-    if (integerPart[i] > '1' || digit % 4 === 0 || i === 0) {
-      const hangulNumber = HANGUL_NUMBERS[Number(integerPart[i])];
+      if (integerPart[i] > '1' || digit % 4 === 0 || i === 0) {
+        const hangulNumber = HANGUL_NUMBERS[Number(integerPart[i])];
 
-      if (hangulNumber) {
-        result.push(hangulNumber);
-        pronunDigits = true;
+        if (hangulNumber) {
+          result.push(hangulNumber);
+          pronunDigits = true;
+        }
+      }
+
+      if (pronunDigits && digit % 4 === 0) {
+        result.push(HANGUL_DIGITS[digit / 4]);
+        pronunDigits = false;
+      }
+
+      if (integerPart[i] !== '0') {
+        result.push(HANGUL_CARDINAL[digit % 4]);
       }
     }
-
-    if (pronunDigits && digit % 4 === 0) {
-      result.push(HANGUL_DIGITS[digit / 4]);
-      pronunDigits = false;
-    }
-
-    if (integerPart[i] !== '0') {
-      result.push(HANGUL_CARDINAL[digit % 4]);
-    }
+    result.push(HANGUL_NUMBERS[Number(integerPart[integerPart.length - 1])]);
   }
-  result.push(HANGUL_NUMBERS[Number(integerPart[integerPart.length - 1])]);
 
   if (decimalPart) {
     result.push('점');
 
     for (let i = 0; i < decimalPart.length; i++) {
-      result.push(HANGUL_NUMBERS[Number(decimalPart[i])]);
+      result.push(HANGUL_NUMBERS_FOR_DECIMAL[Number(decimalPart[i])]);
     }
   }
 
