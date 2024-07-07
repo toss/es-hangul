d6875bd2a7dfe78c2c3d04df59da21c7e437aed6
JunSeong <gentlmanjun@naver.com>
2024-07-07 20:23:48 +0900

    feat: add test case for test coverage

--- src/needAssemble.spec.ts
+++ src/needAssemble.spec.ts
@@ -6,7 +6,8 @@ describe('needAssemble', () => {
   });
   it('온전하지 않지만 합성 불가능한 한글 문장', () => {
     expect(needAssemble('아버지가 방에 들어갑니다ㅏ')).toBe(false);
-    expect(needAssemble('ㄱ아버지가 방에 들어갑니다')).toBe(false);
+    expect(needAssemble('아버지가 방ㅇ에 들어갑니다')).toBe(false);
+    expect(needAssemble('아버지가 ㅇ에 들어갑니다')).toBe(false);
   });
   it('모음과 자음이 나눠져있는 문장', () => {
     expect(needAssemble('아버지가 방ㅇㅔ 들어갑니다')).toBe(true);
