0c087db54f5f1621b8859de21b18eb209c3b53f9
devjojo <sa02045@naver.com>
2024-07-09 22:14:30 +0900

    fix: hangulIncludes invariant

--- src/hangulIncludes.ts
+++ src/hangulIncludes.ts
@@ -1,8 +1,16 @@
 import { disassembleHangul } from './disassemble';
+import { safeParseHangul } from './_internal/hangul';
 
 export function hangulIncludes(x: string, y: string) {
-  const disassembledX = disassembleHangul(x);
-  const disassembledY = disassembleHangul(y);
+  const parsedX = safeParseHangul(x);
+  const parsedY = safeParseHangul(y);
+
+  if (!(parsedX.success && parsedY.success)) {
+    return y.length === 0;
+  }
+
+  const disassembledX = disassembleHangul(parsedX.data);
+  const disassembledY = disassembleHangul(parsedY.data);
 
   return disassembledX.includes(disassembledY);
 }
