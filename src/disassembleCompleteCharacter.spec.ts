4d2472dd24935341ab0bf11246e443445aeadb66
chanhyuk-park <chanhyuk.park@toss.im>
2024-07-13 00:20:40 +0900

    누락된 부분 수정

--- src/disassembleCompleteCharacter.spec.ts
+++ src/disassembleCompleteCharacter.spec.ts
@@ -1,8 +1,8 @@
-import { disassembleCompleteHangulCharacter } from './disassembleCompleteHangulCharacter';
+import { disassembleCompleteCharacter } from './disassembleCompleteCharacter';
 
-describe('disassembleCompleteHangulCharacter', () => {
+describe('disassembleCompleteCharacter', () => {
   it('값', () => {
-    expect(disassembleCompleteHangulCharacter('값')).toEqual({
+    expect(disassembleCompleteCharacter('값')).toEqual({
       first: 'ㄱ',
       middle: 'ㅏ',
       last: 'ㅂㅅ',
@@ -10,7 +10,7 @@ describe('disassembleCompleteHangulCharacter', () => {
   });
 
   it('리', () => {
-    expect(disassembleCompleteHangulCharacter('리')).toEqual({
+    expect(disassembleCompleteCharacter('리')).toEqual({
       first: 'ㄹ',
       middle: 'ㅣ',
       last: '',
@@ -18,7 +18,7 @@ describe('disassembleCompleteHangulCharacter', () => {
   });
 
   it('빚', () => {
-    expect(disassembleCompleteHangulCharacter('빚')).toEqual({
+    expect(disassembleCompleteCharacter('빚')).toEqual({
       first: 'ㅂ',
       middle: 'ㅣ',
       last: 'ㅈ',
@@ -26,7 +26,7 @@ describe('disassembleCompleteHangulCharacter', () => {
   });
 
   it('박', () => {
-    expect(disassembleCompleteHangulCharacter('박')).toEqual({
+    expect(disassembleCompleteCharacter('박')).toEqual({
       first: 'ㅂ',
       middle: 'ㅏ',
       last: 'ㄱ',
@@ -34,7 +34,7 @@ describe('disassembleCompleteHangulCharacter', () => {
   });
 
   it('완전한 한글 문자열이 아니면 undefined를 반환해야 합니다.', () => {
-    expect(disassembleCompleteHangulCharacter('ㄱ')).toBeUndefined;
-    expect(disassembleCompleteHangulCharacter('ㅏ')).toBeUndefined;
+    expect(disassembleCompleteCharacter('ㄱ')).toBeUndefined;
+    expect(disassembleCompleteCharacter('ㅏ')).toBeUndefined;
   });
 });
