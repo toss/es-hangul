410d8fafe996cd64213c746221f084c128bf67be
chanhyuk-park <chanhyuk.park@toss.im>
2024-07-21 16:15:37 +0900

    write test code

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -84,44 +84,6 @@ describe('hasSingleBatchim', () => {
   });
 });
 
-describe('getChoseong', () => {
-  it('"사과" 단어에서 초성 "ㅅㄱ"을 추출한다.', () => {
-    expect(getChoseong('사과')).toBe('ㅅㄱ');
-  });
-  it('"프론트엔드" 단어에서 초성 "ㅍㄹㅌㅇㄷ"을 추출한다.', () => {
-    expect(getChoseong('프론트엔드')).toBe('ㅍㄹㅌㅇㄷ');
-  });
-  it('"ㄴㅈ" 문자에서 초성 "ㄴㅈ"을 추출한다.', () => {
-    expect(getChoseong('ㄴㅈ')).toBe('ㄴㅈ');
-  });
-  it('"리액트" 단어에서 초성 "ㄹㅇㅌ"을 추출한다.', () => {
-    expect(getChoseong('리액트')).toBe('ㄹㅇㅌ');
-  });
-
-  it('"띄어 쓰기" 문장에서 초성 "ㄸㅇ ㅆㄱ"을 추출한다.', () => {
-    expect(getChoseong('띄어 쓰기')).toBe('ㄸㅇ ㅆㄱ');
-  });
-});
-
-describe('getFirstConsonants', () => {
-  it('"사과" 단어에서 초성 "ㅅㄱ"을 추출한다.', () => {
-    expect(getFirstConsonants('사과')).toBe('ㅅㄱ');
-  });
-  it('"프론트엔드" 단어에서 초성 "ㅍㄹㅌㅇㄷ"을 추출한다.', () => {
-    expect(getFirstConsonants('프론트엔드')).toBe('ㅍㄹㅌㅇㄷ');
-  });
-  it('"ㄴㅈ" 문자에서 초성 "ㄴㅈ"을 추출한다.', () => {
-    expect(getFirstConsonants('ㄴㅈ')).toBe('ㄴㅈ');
-  });
-  it('"리액트" 단어에서 초성 "ㄹㅇㅌ"을 추출한다.', () => {
-    expect(getFirstConsonants('리액트')).toBe('ㄹㅇㅌ');
-  });
-
-  it('"띄어 쓰기" 문장에서 초성 "ㄸㅇ ㅆㄱ"을 추출된다.', () => {
-    expect(getFirstConsonants('띄어 쓰기')).toBe('ㄸㅇ ㅆㄱ');
-  });
-});
-
 describe('hasValueInReadOnlyStringList', () => {
   const testReadonlyList = ['ㄱ', 'ㄴ', 'ㄷ'] as const;
 
