0cfbed68edc656e2081785a2e7510900b1f53597
박찬혁 <pgg6713@gmail.com>
2024-07-23 11:59:54 +0900

    feat: `canBeChoseong`, `canBeJungseong`, `canBeJongseong` 을 utils에서 별도 함수로 분리합니다. (#193)
    * canBe series를 별도 함수로 분리합니다
    
    * add change set

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,12 @@
 export { assembleHangul } from './assemble';
-export { chosungIncludes } from './chosungIncludes';
+export { canBeChoseong, canBeJongseong, canBeJungseong } from './canBe';
 export { choseongIncludes } from './choseongIncludes';
+export { chosungIncludes } from './chosungIncludes';
 export { combineHangulCharacter, combineVowels, curriedCombineHangulCharacter } from './combineHangulCharacter';
 export { convertQwertyToHangul, convertQwertyToHangulAlphabet } from './convertQwertyToHangulAlphabet';
 export { disassemble, disassembleToGroups } from './disassemble';
 export { disassembleCompleteCharacter } from './disassembleCompleteCharacter';
+export { getChoseong } from './getChoseong';
 export { josa } from './josa';
 export { removeLastHangulCharacter } from './removeLastHangulCharacter';
-export { getChoseong } from './getChoseong';
-export {
-  canBeChosung,
-  canBeJongsung,
-  canBeJungsung,
-  hasBatchim,
-  hasProperty,
-  hasSingleBatchim,
-  hasValueInReadOnlyStringList,
-} from './utils';
+export { hasBatchim, hasProperty, hasSingleBatchim, hasValueInReadOnlyStringList } from './utils';
--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -1,12 +1,4 @@
-import {
-  canBeChoseong,
-  canBeJongseong,
-  canBeJungseong,
-  hasBatchim,
-  hasProperty,
-  hasSingleBatchim,
-  hasValueInReadOnlyStringList,
-} from './utils';
+import { hasBatchim, hasProperty, hasSingleBatchim, hasValueInReadOnlyStringList } from './utils';
 
 describe('hasBatchim', () => {
   describe('받침이 있다고 판단되는 경우', () => {
@@ -131,83 +123,5 @@ describe('hasProperty', () => {
     } else {
       expectTypeOf(testKey).toEqualTypeOf<string>();
     }
-  });
-});
-
-describe('canBeChoseong', () => {
-  describe('초성이 될 수 있다고 판단되는 경우', () => {
-    it('ㄱ', () => {
-      expect(canBeChoseong('ㄱ')).toBe(true);
-    });
-    it('ㅃ', () => {
-      expect(canBeChoseong('ㅃ')).toBe(true);
-    });
-  });
-
-  describe('초성이 될 수 없다고 판단되는 경우', () => {
-    it('ㅏ', () => {
-      expect(canBeChoseong('ㅏ')).toBe(false);
-    });
-    it('ㅘ', () => {
-      expect(canBeChoseong('ㅘ')).toBe(false);
-    });
-    it('ㄱㅅ', () => {
-      expect(canBeChoseong('ㄱㅅ')).toBe(false);
-    });
-    it('가', () => {
-      expect(canBeChoseong('가')).toBe(false);
-    });
-  });
-});
-
-describe('canBeJungseong', () => {
-  describe('중성이 될 수 있다고 판단되는 경우', () => {
-    it('ㅗㅏ', () => {
-      expect(canBeJungseong('ㅗㅏ')).toBe(true);
-    });
-    it('ㅏ', () => {
-      expect(canBeJungseong('ㅏ')).toBe(true);
-    });
-  });
-
-  describe('중성이 될 수 없다고 판단되는 경우', () => {
-    it('ㄱ', () => {
-      expect(canBeJungseong('ㄱ')).toBe(false);
-    });
-    it('ㄱㅅ', () => {
-      expect(canBeJungseong('ㄱㅅ')).toBe(false);
-    });
-    it('가', () => {
-      expect(canBeJungseong('가')).toBe(false);
-    });
-  });
-});
-
-describe('canBeJongseong', () => {
-  describe('종성이 될 수 있다고 판단되는 경우', () => {
-    it('ㄱ', () => {
-      expect(canBeJongseong('ㄱ')).toBe(true);
-    });
-    it('ㄱㅅ', () => {
-      expect(canBeJongseong('ㄱㅅ')).toBe(true);
-    });
-    it('ㅂㅅ', () => {
-      expect(canBeJongseong('ㅂㅅ')).toBe(true);
-    });
-  });
-
-  describe('종성이 될 수 없다고 판단되는 경우', () => {
-    it('ㅎㄹ', () => {
-      expect(canBeJongseong('ㅎㄹ')).toBe(false);
-    });
-    it('ㅗㅏ', () => {
-      expect(canBeJongseong('ㅗㅏ')).toBe(false);
-    });
-    it('ㅏ', () => {
-      expect(canBeJongseong('ㅏ')).toBe(false);
-    });
-    it('가', () => {
-      expect(canBeJongseong('ㅏ')).toBe(false);
-    });
   });
 });
