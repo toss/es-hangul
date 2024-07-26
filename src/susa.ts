0cdff1fbd067b98ffda09fa04916f16615fa5a2c
bosung baek <teabs1125@gmail.com>
2024-07-26 23:14:36 +0900

    feat: 테스트케이스를 통과하도록  수정

--- src/susa.ts
+++ src/susa.ts
@@ -1,16 +1,41 @@
 import { SUSA_MAP, SUSA_CLASSIFIER_MAP } from './constants';
+import { hasProperty } from './utils';
+
+export function susa(num: number, classifier?: boolean): string {
+  validateNumber(num);
+  return classifier ? getClassifierWord(num) : getNumberWord(num);
+}
+
+function getClassifierWord(num: number): string {
+  if (num === 20) {
+    return SUSA_CLASSIFIER_MAP[num];
+  }
+
+  const tens = Math.floor(num / 10) * 10;
+  const ones = num % 10;
+
+  let classifier = '';
+
+  if (hasProperty(SUSA_MAP, tens)) {
+    classifier += SUSA_MAP[tens];
+  }
+
+  if (hasProperty(SUSA_CLASSIFIER_MAP, ones)) {
+    classifier += SUSA_CLASSIFIER_MAP[ones];
+    return classifier;
+  }
+
+  if (hasProperty(SUSA_MAP, ones)) {
+    classifier += SUSA_MAP[ones];
+  }
+
+  return classifier;
+}
 
 function validateNumber(num: number): void {
   if (Number.isNaN(num) || !Number.isFinite(num) || num <= 0 || num > 100) {
     throw new Error('지원하지 않는 숫자입니다.');
   }
-}
-
-function getClassifier(num: number): string {
-  if (num in SUSA_CLASSIFIER_MAP) {
-    return SUSA_CLASSIFIER_MAP[num as keyof typeof SUSA_CLASSIFIER_MAP];
-  }
-  throw new Error('지원하지 않는 숫자입니다.');
 }
 
 function getNumberWord(num: number): string {
@@ -22,17 +47,12 @@ function getNumberWord(num: number): string {
   const ones = num % 10;
   let result = '';
 
-  if (tens > 0) {
-    result += SUSA_MAP[tens as keyof typeof SUSA_MAP];
+  if (tens > 0 && hasProperty(SUSA_MAP, tens)) {
+    result += SUSA_MAP[tens];
   }
-  if (ones > 0) {
-    result += SUSA_MAP[ones as keyof typeof SUSA_MAP];
+  if (ones > 0 && hasProperty(SUSA_MAP, ones)) {
+    result += SUSA_MAP[ones];
   }
 
   return result;
-}
-
-export function susa(num: number, classifier: boolean): string {
-  validateNumber(num);
-  return classifier ? getClassifier(num) : getNumberWord(num);
 }
