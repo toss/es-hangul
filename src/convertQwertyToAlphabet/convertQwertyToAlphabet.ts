52cc043b42a93b2e3c45555437ae5139d1d56ca0
wook <yho7955@naver.com>
2024-09-16 21:29:59 +0900

    Revert "fix: CHANGELOG.md 공백 제거 및 convertQwertyToAlphabet 오타 수정"
    This reverts commit 44c542110f21705bd574e315ad5ac25f48e3b34a.

--- src/convertQwertyToAlphabet/convertQwertyToAlphabet.ts
+++ src/convertQwertyToAlphabet/convertQwertyToAlphabet.ts
@@ -19,7 +19,7 @@ export function convertQwertyToAlphabet(word: string): string {
 /**
  * @name convertQwertyToHangul
  * @description
- * 영어 알파벳을 qwerty 자판과 매칭되는 한글 문자와 문장으로 변환합니다.
+ * 영어 알파벳을 qwerty 자판과 매칭과는 한글 문자와 문장으로 변환합니다.
  * @param word 한글 문장으로 변환하고자 하는 영문
  * @returns qwerty 영어 알파벳을 변환하여 한글 규칙에 맞게 합성한 문자열
  */
