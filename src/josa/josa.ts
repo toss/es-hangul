fbacc7db53b9317d21339de74a98441d1bfe76a3
chanhyuk-park <chanhyuk.park@toss.im>
2024-10-31 23:14:33 +0900

    revert josa

--- src/josa/josa.ts
+++ src/josa/josa.ts
@@ -1,6 +1,5 @@
-import { COMPLETE_HANGUL_START_CHARCODE, JONGSEONGS, NUMBER_OF_JONGSEONG } from '@/_internal/constants';
-import { hasBatchim } from '../hasBatchim';
 import { disassembleCompleteCharacter } from '@/disassembleCompleteCharacter';
+import { hasBatchim } from '../hasBatchim';
 
 type JosaOption =
   | '이/가'
@@ -38,8 +37,7 @@ function josaPicker(word: string, josa: JosaOption): string {
   const has받침 = hasBatchim(word);
   let index = has받침 ? 0 : 1;
 
-  const is종성ㄹ = has받침 && isJongseongㄹ(word[word.length - 1]);
-  // const is종성ㄹ = disassembleCompleteCharacter(word[word.length - 1])?.jongseong === 'ㄹ';
+  const is종성ㄹ = has받침 && disassembleCompleteCharacter(word[word.length - 1])?.jongseong === 'ㄹ';
 
   const isCaseOf로 = has받침 && is종성ㄹ && 로_조사.includes(josa);
 
@@ -54,9 +52,4 @@ function josaPicker(word: string, josa: JosaOption): string {
   }
 
   return josa.split('/')[index];
-}
-
-function isJongseongㄹ(lastChar: string): boolean {
-  const charCode = lastChar.charCodeAt(0);
-  return (charCode - COMPLETE_HANGUL_START_CHARCODE) % NUMBER_OF_JONGSEONG === JONGSEONGS.indexOf('ㄹ');
 }
