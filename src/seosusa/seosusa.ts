8f72efd90cb85fb462ee8481eea31b8a4297c846
JunSeong <gentlmanjun@naver.com>
2025-01-05 20:56:01 +0900

    fix: update seosusa function can handle more than 100 numbers

--- src/seosusa/seosusa.ts
+++ src/seosusa/seosusa.ts
@@ -1,14 +1,16 @@
+import { numberToHangul } from '@/numberToHangul';
 import { hasProperty } from '../_internal';
 import { SEOSUSA_MAP, SEOSUSA_SPECIAL_CASE_MAP } from './constants';
 
 /**
- * 숫자를 순 우리말 서수사로 변환합니다.
+ * 숫자를 한글 서수사로 변환합니다.
  *
  * @remarks
- * - **서수사**는 순서를 나타내는 우리말 단어입니다. 이 함수에서는 첫째, 둘째, 셋째와 같은 고유어 계통 단어를 다룹니다.
- * - 1부터 99까지의 정수를 서수사 문자열로 변환합니다.
+ * - **서수사**는 순서를 나타내는 단어입니다.
+ * - 1부터 99까지의 정수는 순우리말 서수사 문자열로 변환합니다.
+ * - 100 이상의 정수는 한자어 서수사 문자열로 변환합니다.
  *
- * @param num - 변환할 숫자 (1-99)
+ * @param num - 변환할 숫자
  * @return 변환된 서수사 문자열
  * @throws {Error} 지원하지 않는 숫자인 경우
  *
@@ -25,23 +27,26 @@ import { SEOSUSA_MAP, SEOSUSA_SPECIAL_CASE_MAP } from './constants';
  * seosusa(30); // '서른째'
  * seosusa(40); // '마흔째'
  * seosusa(99); // '아흔아홉째'
+ * seosusa(100); // '백째'
  *
  * @see https://es-hangul.slash.page/docs/api/seosusa
  */
 export function seosusa(num: number): string {
-  if (!isValidNumber(num)) {
-    throw new Error('유효하지 않은 입력입니다. 1부터 99까지의 정수만 지원합니다.');
+  if (num === 0 || !Number.isInteger(num)) {
+    throw new Error('유효하지 않은 입력입니다. 1이상의 정수만 지원합니다.');
   }
-  return `${getOrdinalWord(num)}째`;
+
+  if (num >= 1 && num <= 99) {
+    return `${getOrdinalWord(num)}째`;
+  }
+
+  try {
+    return `${numberToHangul(num)}째`;
+  } catch (error) {
+    throw new Error('유효하지 않은 입력입니다. 1이상의 정수만 지원합니다.');
+  }
 }
 
-/**
- * 유효한 숫자 범위인지 확인합니다.
- * 1부터 99까지의 숫자만 유효합니다.
- */
-function isValidNumber(num: number): boolean {
-  return num >= 1 && num <= 99 && Number.isInteger(num);
-}
 function getOrdinalWord(num: number): string {
   if (hasProperty(SEOSUSA_SPECIAL_CASE_MAP, num)) {
     return SEOSUSA_SPECIAL_CASE_MAP[num];
