28b0667d16a88ff6300724e7e1aecb2474924fc5
dewy <dewy@tossinvest.com>
2025-02-17 19:55:20 +0900

    chore: edit test cases

--- src/convertHangulToQwerty/convertHangulToQwerty.spec.ts
+++ src/convertHangulToQwerty/convertHangulToQwerty.spec.ts
@@ -4,17 +4,19 @@ describe('convertQwertyToHangul', () => {
   it('한글을 알파벳으로 변환한다.', () => {
     expect(convertHangulToQwerty('뮻')).toBe('abc');
     expect(convertHangulToQwerty('겨노')).toBe('rush');
+    expect(convertHangulToQwerty('님노')).toBe('slash');
+    expect(convertHangulToQwerty('ㅙㅜㅎ 햐ㅣ애ㅜㅎ')).toBe('hong gildong');
   });
 
   it('쌍자음 및 이중모음도 그에 대응되는 영문 알파벳으로 변환한다.', () => {
     expect(convertHangulToQwerty('쨰ㅉ')).toBe('WOW');
-    expect(convertHangulToQwerty('빠른 논의')).toBe('Qkfms shsdml');
     expect(convertHangulToQwerty('빠짜따까싸')).toBe('QkWkEkRkTk');
     expect(convertHangulToQwerty('과궈괴귀긔')).toBe('rhkrnjrhlrnlrml');
   });
 
   it('영문, 숫자 등 한글이 아닌 글자는 그대로 유지한다.', () => {
     expect(convertHangulToQwerty('FE개발!')).toBe('FEroqkf!');
+    expect(convertHangulToQwerty('ㄴ대ㅕㅣ, ㅏㅐㄱㄷㅁ')).toBe('seoul, korea');
   });
 
   it('한글 음소 또한 알파벳으로 변환한다.', () => {
