fd79584887e8983141391b44179f6abeda571bd7
gwagjiug <kwjo0228@naver.com>
2025-04-03 15:51:12 +0900

    chore: convertHangulToQwerty 함수에 예제 추가

--- src/convertHangulToQwerty/convertHangulToQwerty.ts
+++ src/convertHangulToQwerty/convertHangulToQwerty.ts
@@ -8,6 +8,12 @@ import { HANGUL_TO_QWERTY_KEYBOARD_MAP } from './constants';
  * 한글을 qwerty 자판과 매칭되는 영어 알파벳으로 변환합니다.
  * @param word 영문으로 변환하고자 하는 한글
  * @returns 한글을 규칙에 맞게 qwerty 자판의 영어 알파벳으로 변환한 문자열
+ * @example
+ * convertHangulToQwerty('겨노'); // 'rush'
+ * convertHangulToQwerty('쨰ㅉ'); // 'WOW'
+ * convertHangulToQwerty('FE개발!'); // 'FEroqkf!'
+ * convertHangulToQwerty('ㅇPdml'); // 'dPdml'
+ * convertHangulToQwerty(''); // ''
  */
 export function convertHangulToQwerty(word: string): string {
   if (!word) {
