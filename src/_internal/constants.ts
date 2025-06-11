df889f3f564268e556f019f79d5671da9eca52e6
grimza99 <gbtmxlf@naver.com>
2025-06-11 18:44:12 +0900

    Chore : ALPHABET_TO_KOREAN 상수 생성

--- src/_internal/constants.ts
+++ src/_internal/constants.ts
@@ -161,3 +161,32 @@ export const HANGUL_DIGITS_MAX = HANGUL_DIGITS.length * 4;
 export const HANGUL_NUMBERS = ['', '일', '이', '삼', '사', '오', '육', '칠', '팔', '구'];
 export const HANGUL_NUMBERS_FOR_DECIMAL = ['영', '일', '이', '삼', '사', '오', '육', '칠', '팔', '구'];
 export const HANGUL_CARDINAL = ['', '십', '백', '천'];
+
+export const ALPHABET_TO_KOREAN: Record<string, string> = {
+  A: '에이',
+  B: '비',
+  C: '씨',
+  D: '디',
+  E: '이',
+  F: '에프',
+  G: '지',
+  H: '에이치',
+  I: '아이',
+  J: '제이',
+  K: '케이',
+  L: '엘',
+  M: '엠',
+  N: '엔',
+  O: '오',
+  P: '피',
+  Q: '큐',
+  R: '알',
+  S: '에스',
+  T: '티',
+  U: '유',
+  V: '브이',
+  W: '더블유',
+  X: '엑스',
+  Y: '와이',
+  Z: '지',
+};
